c620671bc5ec074402bfe9eae0763f3443722e1f
Douglas Liu <49981614+yzakzero@users.noreply.github.com>
2021-01-03 11:20:43 +0800

    Update K-means.py

--- K-means.py
+++ K-means.py
@@ -1,26 +1,65 @@
-#https://zhuanlan.zhihu.com/p/111180811
 import time
 import matplotlib.pyplot as plt
 import matplotlib
-from  sklearn.cluster import KMeans
-from sklearn.datasets import load_iris 
 matplotlib.rcParams['font.sans-serif'] = [u'SimHei']
 matplotlib.rcParams['axes.unicode_minus'] = False
+import numpy as np
+def distEclud(vecA,vecB):
+    """
+    计算两个向量的欧式距离
+    """
+    return np.sqrt(np.sum(np.power(vecA-vecB,2)))
 
-# 获取鸢尾花数据集，特征分别是sepal length、sepal width、petal length、petal width
-iris = load_iris() 
-X = iris.data[:,2:]  # 通过花瓣的两个特征来聚类
-k=3  # 假设聚类为3类
-# 构建模型
+def randCent(dataSet,k):
+    """
+    随机生成k个点作为质心，其中质心均在整个数据数据的边界之内
+    """
+    n=dataSet.shape[1] # 获取数据的维度
+    centroids = np.mat(np.zeros((k,n)))
+    for j in range(n):
+        minJ = np.min(dataSet[:,j])
+        rangeJ = np.float(np.max(dataSet[:,j])-minJ)
+        centroids[:,j] = minJ+rangeJ*np.random.rand(k,1)
+    return centroids
+
+def kMeans(dataSet,k,distMeas=distEclud, createCent=randCent):
+    """
+    k-Means聚类算法,返回最终的k各质心和点的分配结果
+    """
+    m = dataSet.shape[0]  #获取样本数量
+    # 构建一个簇分配结果矩阵，共两列，第一列为样本所属的簇类值，第二列为样本到簇质心的误差
+    clusterAssment = np.mat(np.zeros((m,2)))
+    # 1. 初始化k个质心
+    centroids = createCent(dataSet,k) 
+    clusterChanged = True
+    while clusterChanged:
+        clusterChanged = False
+        for i in range(m):
+            minDist = np.inf
+            minIndex = -1
+            # 2. 找出最近的质心
+            for j in range(k):
+                distJI = distMeas(centroids[j,:],dataSet[i,:])
+                if distJI < minDist:
+                    minDist = distJI
+                    minIndex = j
+            # 3. 更新每一行样本所属的簇
+            if clusterAssment[i,0] != minIndex:
+                clusterChanged = True
+            clusterAssment[i,:]=minIndex,minDist**2
+        print(centroids) # 打印质心
+        # 4. 更新质心
+        for cent in range(k):
+            ptsClust = dataSet[np.nonzero(clusterAssment[:,0].A==cent)[0]] # 获取给定簇的所有点
+            centroids[cent,:] = np.mean(ptsClust,axis=0) # 沿矩阵列的方向求均值
+    return centroids,clusterAssment
+
 s=time.time()
-km = KMeans(n_clusters=k) 
-km.fit(X)
-print("用sklearn内置的K-Means算法聚类耗时：",time.time()-s)
-
-label_pred = km.labels_   # 获取聚类后的样本所属簇对应值
-centroids = km.cluster_centers_  # 获取簇心
-
-#绘制K-Means结果
+myCentroids,clustAssing=kMeans(X,3)  # myCentroids为簇质心
+print("用K-Means算法原理聚类耗时：",time.time()-s)
+centroids=myCentroids.A # 将matrix转换为ndarray类型
+# 获取聚类后的样本所属的簇值，将matrix转换为ndarray
+y_kmeans=clustAssing[:,0].A[:,0]
 # 未聚类前的数据分布
 plt.subplot(121)
 plt.scatter(X[:, 0], X[:, 1], s=50)
@@ -30,9 +69,9 @@ plt.title("未聚类前的数据分布")
 plt.subplots_adjust(wspace=0.5)
 
 plt.subplot(122)
-plt.scatter(X[:, 0], X[:, 1], c=label_pred, s=50, cmap='viridis')
-plt.scatter(centroids[:,0],centroids[:,1],c='red',marker='o',s=100)
+plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=50, cmap='viridis')
+plt.scatter(centroids[:, 0], centroids[:, 1], c='red', s=100, alpha=0.5)
 plt.xlabel('petal length')
 plt.ylabel('petal width')
-plt.title("用sklearn内置的K-Means算法聚类结果")
+plt.title("用K-Means算法原理聚类的效果")
 plt.show()
